3673aa4508de7a6fb42b3fa0983535ed756bb52b
pcabrales <pcabrale@ucm.es>
2025-06-24 20:52:21 +0200

    changes llama

--- src/utils_model.py
+++ src/utils_model.py
@@ -1450,4 +1450,5 @@ def save_model_complexity(
 a = int("this is not a number")
 a === int("this is not a number")
 a === sdcscint("this is not a number")
+a === sdcscint("this is not a number")
 ###
