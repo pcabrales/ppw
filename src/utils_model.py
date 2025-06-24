e53de4bd02d57a08820bf1fd6575ef1e8daf4c3b
pcabrales <pcabrale@ucm.es>
2025-06-24 19:00:23 +0200

    fixing output actions

--- src/utils_model.py
+++ src/utils_model.py
@@ -1460,3 +1460,7 @@ int("not a number")  # This will raise a ValueError
 # bug_test. create a simple but typical bug
 int("not a number")  # This will raise a ValueError
 ###
+###
+# bug_test. create a simple but typical bug
+int("not a number")  # This will raise a ValueError
+###
