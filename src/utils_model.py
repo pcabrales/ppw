2f888e16906f6e46811977d7c6be09973d55e96e
pcabrales <pcabrale@ucm.es>
2025-06-24 19:30:54 +0200

    changes

--- src/utils_model.py
+++ src/utils_model.py
@@ -1472,3 +1472,6 @@ int("not a number")  # This will raise a ValueError
 # bug_test. create a simple but typical bug
 int("not a number")  # This will raise a ValueError
 ###
+# bug_test. create a simple but typical bug
+int("not a number")  # This will raise a ValueError
+###
